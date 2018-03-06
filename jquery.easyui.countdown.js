e1a7156338a7e2fb29d2c90716cdaa8497c11087
蔡海斌 <243008827@qq.com>
2018-03-06 16:45:38 +0800

    Update jquery.easyui.countdown.js

--- jquery.easyui.countdown.js
+++ jquery.easyui.countdown.js
@@ -1,3 +1,7 @@
+/*******************************************************************************
+ * @author caihaibin <243008827@qq.com>
+ * @version 1.1.1 
+ *******************************************************************************/
 (function($) {
 
 	function getTxt(target) {
@@ -47,7 +51,7 @@
 				if(time <= 0) {
 					//统一调用计时器插件结束动作
 					$(target).countdown('stop');
-					//$(target).data("clearTime")();
+					$.data(target, 'countdown').options.onEnd();
 					return;
 				}
 				$.data(target, 'countdown').options.onShowtime(target, time, time + '秒');
@@ -71,7 +75,8 @@
 	function parseOptions(target) {
 		var t = $(target);
 		return $.extend({}, {
-			time: t.attr('time')
+			//如果参数内容为空或undefined,不会覆盖前面的内容.如果有值,依次会覆盖前面数组的具体键值
+			time: t.attr('time') ? t.attr('time') : undefined
 		});
 	}
 	$.fn.countdown = function(options, param) {
@@ -87,10 +92,14 @@
 			//实现针对每元素级的函数和变量
 			var state = $.data(this, 'countdown');
 			if(state) {
+				//二次初始化,只覆盖变量,重新绑定事件等..
 				$.extend(state.options, options);
 			} else {
 				state = $.data(this, 'countdown', {
+					//该对象所有选项,包括配置参数,传入配置,都存放在options
+					//如果参数内容为空或undefined,不会覆盖前面的内容.如果有值,依次会覆盖前面数组的具体键值
 					options: $.extend({}, $.fn.countdown.defaults, parseOptions(this), options),
+					//该对象所有临时变量
 					data: {
 						//按钮文本内容,用于复原
 						text: getTxt(this),
@@ -99,29 +108,49 @@
 					}
 				});
 			}
+			//创建元素,只针对元素部分的调整.需要区分事件和元素的操作
 			create(this);
+			//绑定相关事件,可重复绑定;重复绑定时,通过事件命名空间进行解除
 			bindEvents(this);
 		});
 	};
-
+	//开放给外部调用动作
+	//注:函数内部需要二次调用闭包中所定义函数,jq对象为数组,请求的闭包中的函数动作是单体.
 	$.fn.countdown.methods = {
+		//解析出参数配置
 		parseOptions: function(jq) {
+			//参数只需要一份,列表对象中参数相同,所以只需要返回首个
 			return parseOptions(jq[0]);
 		},
+		//返回所配置参数
 		options: function(jq) {
+			//参数只需要一份,列表对象中参数相同,所以只需要返回首个
 			return $.data(jq[0], 'countdown').options;
 		},
+		//停止计时动作
 		stop: function(jq) {
 			return jq.each(function() {
 				stop(this);
 			});
 		}
 	};
-
+	//外部参数配置,允许参数和函数事件等
 	$.fn.countdown.defaults = {
+		/**
+		 * 默认时长
+		 */
 		time: 60,
-		onShowtime: function(target, time, txt) {
-			console.log(target, time, txt);
-		}
+		/**
+		 * 计时事件
+		 * @param {Object} target
+		 * @param {Object} time 当前秒数
+		 * @param {Object} txt 秒数文本
+		 */
+		onShowtime: function(target, time, txt) {},
+		/**
+		 * 结束事件
+		 * @param {Object} target
+		 */
+		onEnd: function(target) {}
 	};
 })(jQuery);
